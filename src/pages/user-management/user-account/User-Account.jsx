dcd4b28ba7183c485f41689818c01609c8fc2714
heavynametalpa@gmail.com <heavynametalpa@gmail.com>
2022-12-20 14:17:44 +0800

    12-20-2022

--- src/pages/user-management/user-account/User-Account.jsx
+++ src/pages/user-management/user-account/User-Account.jsx
@@ -1,35 +1,72 @@
-import React from 'react'
-import { ScrollFeature } from '../../../utils/Scroll-Feature'
-import { Button, Flex, Input, Select, Table, Tbody, Td, Th, Thead, Tr, VStack } from '@chakra-ui/react'
-import { Pagination, PaginationContainer, PaginationNext, PaginationPage, PaginationPageGroup, PaginationPrevious } from '@ajna/pagination'
-import { useFetchUsersApi } from './api/user-account.services'
+import React from "react";
+import { ScrollFeature } from "../../../utils/Scroll-Feature";
+import {
+  Button,
+  Flex,
+  Input,
+  Select,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  VStack,
+} from "@chakra-ui/react";
+import {
+  Pagination,
+  PaginationContainer,
+  PaginationNext,
+  PaginationPage,
+  PaginationPageGroup,
+  PaginationPrevious,
+} from "@ajna/pagination";
+import { useFetchUsersApi } from "./api/user-account.services";
+import { usePageUtilities } from "../../../utils/Page-Utilities";
 
 export const UserAccount = () => {
+  const userStatus = true;
+  const pageNumber = 1;
+  const searcha = "";
+  const pageSizea = 10;
 
-  const userStatus = true
-  const pageNumber = 1
-  const search = ''
-  const pageSize = 10
+  const { data } = useFetchUsersApi(userStatus, pageNumber, pageSizea, searcha);
+  const {
+    currentPage,
+    setCurrentPage,
+    pagesCount,
+    pages,
+    setPageSize,
+    pageSize,
+    pageTotal,
+    setPageTotal,
+    search,
+    setSearch,
+    status,
+    setStatus,
+  } = usePageUtilities();
 
-  const { data } = useFetchUsersApi(userStatus, pageNumber, pageSize, search)
-  
-  console.log(data)
-  
+  console.log(data);
+
   return (
-    <VStack h='91.5vh' bgColor='primary' p={5}>
-
-      <Flex w='full' justifyContent='space-between'>
-        <Input w='auto' />
-        <Select w='auto'>
-          <option>1</option>
-          <option>sadasdsdsa</option>
+    <VStack h="91.5vh" bgColor="primary" p={5}>
+      <Flex w="full" justifyContent="space-between">
+        <Input w="auto" onChange={(e) => setSearch(e.target.value)} />
+        <Select w="auto">
+          <option>Active</option>
+          <option>Inactive</option>
         </Select>
       </Flex>
 
-      <Flex w='full'>
-        <ScrollFeature minHeight='50vh' maxHeight='70vh'>
-          <Table variant='simple' size='sm'>
-            <Thead position='sticky' zIndex='docked' top={0} bgColor='secondary'>
+      <Flex w="full" flexDirection="column" gap={5}>
+        <ScrollFeature maxHeight="70vh">
+          <Table variant="simple" size="sm">
+            <Thead
+              position="sticky"
+              zIndex="docked"
+              top={0}
+              bgColor="secondary"
+            >
               <Tr>
                 <Th>Hi</Th>
               </Tr>
@@ -41,40 +78,51 @@ export const UserAccount = () => {
             </Tbody>
           </Table>
         </ScrollFeature>
-      </Flex>
-
-      <Flex w='full' justifyContent='space-between'>
-        <Button size='sm' leftIcon={'icon'}>New User</Button>
-        <Pagination
-        // pagesCount={pagesCount}
-        // currentPage={currentPage}
-        // onPageChange={handlePageChange}
-        >
-          <PaginationContainer gap={1}>
-            <PaginationPrevious bgColor='primary' color='myWhite' size='sm' _hover={{ bgColor: 'myWhite', color: 'primary' }}>{`<<`}</PaginationPrevious>
-            <PaginationPageGroup>
-              {/* {pages.map((page) => (
+        <Flex w="full" justifyContent="space-between">
+          <Button size="sm" leftIcon={"icon"}>
+            New User
+          </Button>
+          <Pagination
+          pagesCount={pagesCount}
+          currentPage={currentPage}
+          // onPageChange={handlePageChange}
+          >
+            <PaginationContainer gap={1}>
+              <PaginationPrevious
+                bgColor="primary"
+                color="myWhite"
+                size="sm"
+                _hover={{ bgColor: "myWhite", color: "primary" }}
+              >{`<<`}</PaginationPrevious>
+              <PaginationPageGroup>
+                {pages.map((page) => (
                 <PaginationPage
                   size='sm' bgColor='primary' color='myWhite' _hover={{ bgColor: 'myWhite', color: 'primary' }} px={2}
                   key={`pagination_page_${page}`}
                   page={page}
                 />
-              ))} */}
-            </PaginationPageGroup>
-            <PaginationNext bgColor='primary' color='myWhite' size='sm' _hover={{ bgColor: 'myWhite', color: 'primary' }}>{`>>`}</PaginationNext>
-            <Select
-              // onChange={handlePageSizeChange}
-              w='40%' size='sm'
-            >
-              <option value={Number(5)}>5</option>
-              <option value={Number(10)}>10</option>
-              <option value={Number(25)}>25</option>
-              <option value={Number(50)}>50</option>
-            </Select>
-          </PaginationContainer>
-        </Pagination>
+              ))}
+              </PaginationPageGroup>
+              <PaginationNext
+                bgColor="primary"
+                color="myWhite"
+                size="sm"
+                _hover={{ bgColor: "myWhite", color: "primary" }}
+              >{`>>`}</PaginationNext>
+              <Select
+                // onChange={handlePageSizeChange}
+                w="40%"
+                size="sm"
+              >
+                <option value={Number(5)}>5</option>
+                <option value={Number(10)}>10</option>
+                <option value={Number(25)}>25</option>
+                <option value={Number(50)}>50</option>
+              </Select>
+            </PaginationContainer>
+          </Pagination>
+        </Flex>
       </Flex>
-
     </VStack>
-  )
-}
+  );
+};
